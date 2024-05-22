cedb8bdf4fef8a9cc0a2101bef124ec61a0131c8
Arthur Antonello <arthur.antonello@hotmail.com>
2024-05-22 17:16:07 -0300

    22.05
    Criação das ondas

--- src/pages/Inicio.vue
+++ src/pages/Inicio.vue
@@ -1,6 +1,16 @@
 <template>
   <q-page class="flex flex-center">
-    <p>Inicio</p>
+    <section>
+      <p>Bem vindo ao</p>
+      <img class="logo" src="../assets/aqualert-logo.png" alt="Aqualert">
+    </section>
+    <section class="ondas">
+      <div class="wave wave1"></div>
+      <div class="wave wave2"></div>
+      <div class="wave wave3"></div>
+      <div class="wave wave4"></div>
+    </section>
+    <section class="box"></section>
   </q-page>
 </template>
 
