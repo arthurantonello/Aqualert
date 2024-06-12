86115f3b17de8861e83ba416ed84aa6d730e9d19
Caroline Bendl <106622156+carolbendl@users.noreply.github.com>
2024-06-11 21:46:14 -0300

    script cadastro 1
    Foi adicionado parte do script no cadastro 1

--- src/pages/Cadastro1.vue
+++ src/pages/Cadastro1.vue
@@ -1,40 +1,48 @@
 <template class="inicio">
   <q-page class="flex flex-center fundo-degrade-maior">
-  <section class="cadastro-container">
-    <section class="cadastro-conteudo">
-
-        <img class="cadastro-logo" src="../assets/aqualert-logo.png" alt="Aqualert" />
+    <section class="cadastro-container">
+      <section class="cadastro-conteudo">
+        <img
+          class="cadastro-logo"
+          src="../assets/aqualert-logo.png"
+          alt="Aqualert"
+        />
 
         <div class="cadastro-barra">
           <div class="barra-circulo-check">
-            <q-icon name="check" size="xs"/>
+            <q-icon name="check" size="xs" />
           </div>
           <div class="barra-linha"></div>
           <div class="barra-circulo-uncheck"></div>
         </div>
 
-        <div class="cadastro-formulario q-pa-lg" style="max-width: 315px max-heigth: 370px">
-          <q-form
-            @submit="onSubmit"
-            @reset="onReset"
-            class="q-gutter-auto"
-          >
+        <div
+          class="cadastro-formulario q-pa-lg"
+          style="max-width: 315px max-heigth: 370px"
+        >
+          <q-form @click="onSubmit" class="q-gutter-auto">
             <q-input
               size="50px"
               filled
               v-model="name"
               label="Nome"
               lazy-rules
-              :rules="[ val => val && val.length > 0 || 'Por favor, insira seu nome']"
+              :rules="[
+                (val) =>
+                  (val && val.length > 0) || 'Por favor, insira seu nome',
+              ]"
             />
 
             <q-input
               filled
               clearable
-              v-model="name"
+              v-model="surname"
               label="Sobrenome"
               lazy-rules
-              :rules="[ val => val && val.length > 0 || 'Por favor, insira seu sobrenome']"
+              :rules="[
+                (val) =>
+                  (val && val.length > 0) || 'Por favor, insira seu sobrenome',
+              ]"
             />
 
             <q-input
@@ -48,25 +56,63 @@
             <q-input
               class="cadastro-formulario-senha"
               label="Confirme sua senha"
-              v-model="password"
+              v-model="checkpassword"
               filled
               type="password"
             />
 
             <div>
-              <q-btn to="/cadastro2" label="Continuar" type="submit" color="primary" />
-              <q-btn to="/" label="Voltar" color="primary" flat class="q-ml-sm" />
+              <q-btn
+                to="/cadastro2"
+                label="Continuar"
+                type="submit"
+                color="primary"
+              />
+              <q-btn
+                to="/"
+                label="Voltar"
+                color="primary"
+                flat
+                class="q-ml-sm"
+              />
             </div>
           </q-form>
         </div>
       </section>
-  </section>
+    </section>
   </q-page>
 </template>
 
 <script setup>
-  import { ref } from 'vue'
-  defineOptions({
-    name: 'IndexPage'
-  });
+defineOptions({
+  name: "IndexPage",
+});
+</script>
+
+<script>
+import { useQuasar } from "quasar";
+import { ref } from "vue";
+export default {
+  data() {
+    const $q = useQuasar();
+
+    const name = ref(null);
+    const surname = ref(null);
+    const password = ref(null);
+    const checkpassword = ref(null);
+    const accept = ref(false);
+
+    return {
+      name,
+      surname,
+      password,
+      checkpassword,
+      accept,
+
+      onSubmit() {
+        console.log("ta dos guri");
+      },
+    };
+  },
+};
 </script>
